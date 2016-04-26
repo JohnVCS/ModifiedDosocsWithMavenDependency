2be5770360eb2bbd837af263a9555e19112542b1
jmoseman01 <jmoseman01@gmail.com>
2016-04-26 18:05:35 -0500

    delete contents test

--- tests/test_mavenDepUtil.py
+++ tests/test_mavenDepUtil.py
@@ -1,6 +1,25 @@
 #awesome test
-def test_createTempDirectoryIfDoesntExist():
-    from dosocs2 import mavenDepUtil
-    import os
+from dosocs2 import mavenDepUtil
+import os
+
+def test_directory_get_created_in_tmp_createTempDirectoryIfDoesntExist():
     mavenDepUtil.createTempDirectoryIfDoesntExist()
     assert os.path.exists(r'/tmp/mydep')
+
+def test_directory_get_created_in_tmp_createTempDirectoryIfDoesntExist():
+    #create mydep directory
+    mavenDepUtil.createTempDirectoryIfDoesntExist()
+    
+    #create file1 in mydep directory
+    filename="/tmp/mydep/file1"
+    with open(filename, 'a'):
+        os.utime(filename, None)
+        
+    assert os.path.exists(r'/tmp/mydep')
+    assert os.path.isfile(r'/tmp/mydep/file1')
+    
+    
+    #contents of directory should be deleted after method is ran
+    mavenDepUtil.createTempDirectoryIfDoesntExist()
+    assert not os.path.isfile(r'/tmp/mydep/file1')
+    
